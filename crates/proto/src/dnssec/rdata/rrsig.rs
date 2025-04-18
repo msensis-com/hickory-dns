5b3ec067225a5317ab979195ccb91a9bfbb1ed8b
ArjixWasTaken <me@arjix.dev>
2025-04-18 13:58:29 +0300

    add more serde fixes

--- crates/proto/src/dnssec/rdata/rrsig.rs
+++ crates/proto/src/dnssec/rdata/rrsig.rs
@@ -24,6 +24,7 @@ use super::{DNSSECRData, SIG};
 
 /// RRSIG is really a derivation of the original SIG record data. See SIG for more documentation
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+#[cfg_attr(feature = "serde", serde(transparent))]
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct RRSIG(SIG);
 
